3fd37041e4ead3edea228233decf48cf545554f6
Aaron <agoldman12@gmail.com>
2018-09-03 12:31:05 -0500

    Update hatconfigs.js
    I removed the function that was wrapping around the 'insertAdjacentHTML' method. This allowed the markup to be rendered on my page. I also added id="${hat.id} to the button. This gave each 'add to cart' button an ID attribute that I used in the event listener so that you can only use the 'add to cart' button if that particular hat has been selected.

--- shoppingCart/configs/hatconfigs.js
+++ shoppingCart/configs/hatconfigs.js
@@ -2,7 +2,7 @@ const hatConfig = [
     {
         name: 'Oakland A\'s',
         price: 10.99,
-        img : "../img/AsHat.jpeg",
+        img : "img/AsHat.jpeg",
         alt : "Oakland A's baseball hat",
         id : '1',
         quantity : 0
@@ -10,7 +10,7 @@ const hatConfig = [
     {
         name: 'Milwaukee Brewers',
         price: 10.99,
-        img : "../img/brewersHat2.jpeg",
+        img : "img/brewersHat2.jpeg",
         alt : "Milwaukee Brewers baseball hat",
         id : '2',
         quantity : 0
@@ -18,7 +18,7 @@ const hatConfig = [
     {
         name : 'Kansas City Chiefs',
         price : 10.99,
-        img : "../img/chiefsHat.jpeg",
+        img : "img/chiefsHat.jpeg",
         alt : "Kansas City Chiefs baseball hat",
         id : '3',
         quantity : 0
@@ -26,7 +26,7 @@ const hatConfig = [
     {
         name : 'Grateful Dead',
         price : 10.99,
-        img : "../img/deadHat.jpg",
+        img : "img/deadHat.jpg",
         alt : "Grateful Dead baseball hat",
         id : '4',
         quantity : 0
@@ -34,7 +34,7 @@ const hatConfig = [
     {
          name : 'Detroit Tigers',
          price : 10.99,
-         img : "../img/detroitHat.jpeg",
+         img : "img/detroitHat.jpeg",
          alt : "Detroit Tigers baseball hat",
          id : '5',
          quantity : 0
@@ -42,7 +42,7 @@ const hatConfig = [
     {
          name : 'Cleveland Indians',
          price : 10.99,
-         img : "../img/indiansHat2.jpeg",
+         img : "img/indiansHat2.jpeg",
          alt : "Cleveland Inians baseball hat",
          id : '6',
          quantity : 0
@@ -50,7 +50,7 @@ const hatConfig = [
     {
          name : 'Seattle Mariners',
          price : 10.99,
-         img : "../img/marinersHat.jpeg",
+         img : "img/marinersHat.jpeg",
          alt : "Seattle Mariners baseball hat",
          id : '7',
          quantity : 0
@@ -58,7 +58,7 @@ const hatConfig = [
     {
          name : 'New York Mets',
          price : 10.99,
-         img : "../img/metsHat.jpeg",
+         img : "img/metsHat.jpeg",
          alt : "New York Mets baseball hat",
          id : '8',
          quantity : 0
@@ -66,15 +66,15 @@ const hatConfig = [
     {
          name : 'Baltimore Orioles',
          price : 10.99,
-         img : "../img/oriolesHat2.jpeg",
-         alt : "B'alt'imore Orioles baseball hat",
+         img : "img/oriolesHat2.jpeg",
+         alt : "Baltimore Orioles baseball hat",
          id : '9',
          quantity : 0
     },
     {
          name : 'San Diego Padres',
          price : 10.99,
-         img : "../img/padresHat.jpeg",
+         img : "img/padresHat.jpeg",
          alt : "San Diego Padres baseball hat",
          id : '10',
          quantity : 0
@@ -82,7 +82,7 @@ const hatConfig = [
     {
          name : 'Texas Rangers',
          price : 10.99,
-         img : "../img/rangerHat2.jpeg",
+         img : "img/rangerHat2.jpeg",
          alt : "Texas Rangers baseball hat",
          id : '11',
          quantity : 0
@@ -90,7 +90,7 @@ const hatConfig = [
     {
          name : 'Tampa Bay Rays',
          price : 10.99,
-         img : "../img/raysHat.jpeg",
+         img : "img/raysHat.jpeg",
          alt : "Tampa Bay Rays baseball hat",
          id : '12',
          quantity : 0
@@ -98,7 +98,7 @@ const hatConfig = [
     {
          name : 'Cincinnati Reds',
          price : 10.99,
-         img : "../img/redsHat.jpeg",
+         img : "img/redsHat.jpeg",
          alt : "Cincinnati Reds baseball hat",
          id : '13',
          quantity : 0
@@ -106,7 +106,7 @@ const hatConfig = [
     {
          name : 'Boston Red Sox',
          price : 10.99,
-         img : "../img/redSoxHat2.jpeg",
+         img : "img/redSoxHat2.jpeg",
          alt : "Boston Red Sox baseball hat",
          id : '14',
          quantity : 0
@@ -114,7 +114,7 @@ const hatConfig = [
     {
          name : 'Kansas City Royals',
          price : 10.99,
-         img : "../img/royalsHat2.jpeg",
+         img : "img/royalsHat2.jpeg",
          alt : "Kansas City Royals baseball hat",
          id : '15',
          quantity : 0
@@ -125,48 +125,53 @@ const hatConfig = [
 
 
 //put hat tiles on the page
-function renderMarkup(hatConfig) {
     hatConfig.forEach(function(hat) {
-    const wrapperElement = document.querySelector('#hatContainer');
-    console.log(wrapperElement);
-        wrapperElement.insertAdjacentHTML('afterbegin', `
-        	<div class="col">
-			<h1>${hat.name}</h1>
-			<'img' class="hatimg" src="${hat.img}" 'alt'="${hat.alt}"/>
-			<div class="prodSelection">
-				<p class="price-js">price: ${hat.price}</p>
-				<p>quantity:</p>
-				<select data-id="id">
-					<option value="0">0</option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-				</select>
-				<input class="btn" type="button" name="add" value="Add to cart" />
-				<input class="btn btn--remove" type="button" name="remove" value="Remove" />
-			</div><!--end .prodSelection-->
-		</div><!--end .col -->
-        `)
-    })
-};
+        const wrapperElement = document.getElementById('hatContainer');
+            wrapperElement.insertAdjacentHTML('afterbegin', `
+            <div class="col">
+    			<h1>${hat.name}</h1>
+    			<img class="hatImg" src="${hat.img}" alt="${hat.alt}"/>
+    			<div class="prodSelection">
+    				<p>Price: $${hat.price}</p>
+    				<p>Quantity:</p>
+    				<select data-id="${hat.id}">
+    					<option value="0">0</option>
+    					<option value="1">1</option>
+    					<option value="2">2</option>
+    					<option value="3">3</option>
+    					<option value="4">4</option>
+    					<option value="5">5</option>
+    				</select>
+    				<input class="btn" type="button" id="${hat.id}" name="add" value="Add to cart" />
+    			</div><!--end .prodSelection-->
+    		</div><!--end .col -->
+            `)
+    });
 
+
+
+//event listener and function to update the quantity property of the hat object
+//according to the value selected with the drop down menu.
 document.addEventListener('change', function(e) {
     // get hat id from e.target.dataset
-    let selectedHat = e.target.dataset.id;
+    const selectedHatID = e.target.dataset.id;
 
     //loop through all of our hat objects in our config
     hatConfig.forEach(function(hat){
-       if(selectedHat == hat.id){
-         hatConfig.quantity = selectedHat.value;
-         console.log(hat.quantity);
-     }
-    
+       //if the id of the select menu matches the id of the hat object
+       //update the hat quantity with the value of the select menu.
+       if(selectedHatID === hat.id){
+         hat.quantity = e.target.value;
+        }
     });
-        // when you find the hat with the id same as hat id from select in step 1 of this function, 
-        //update the quantity value with the value of the select
-    
 });
- 
+
+
+/*
+Question: 
+1. When I removed the 'renderMarkup' function the hat tiles were inserted using
+'insertAdjacentHTML()', however they were inserted in the reverse order from how they appear in
+the hatConfigs array. I am not sure why that happened.
+*/
+
 
