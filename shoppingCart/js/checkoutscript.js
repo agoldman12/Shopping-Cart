026eb17e2ce6043c6ae5c3952814c47055b5a654
Aaron <agoldman12@gmail.com>
2018-10-06 15:22:23 -0500

    Update checkoutscript.js
    1. Rewrote getShippingInfo() function. The end result is the same. It gets the shipping inputs / values and reduces them into an array.

--- shoppingCart/js/checkoutscript.js
+++ shoppingCart/js/checkoutscript.js
@@ -3,18 +3,19 @@
 function getShippingInfo() {
 
     const formEl = document.querySelector('.js-shipping-form');
+    const inputEl = Array.from(formEl.querySelectorAll('input'));
+    const initialValue = {};
+
+    const reducer = function(accum, inputEl){
+        if(formEl){
+          accum[inputEl.name] = inputEl.value;
+          return accum;  
+        }
+        
+    };
+    var result = inputEl.reduce(reducer, initialValue);
     
-    if (formEl) {
-        const inputEls = Array.from(formEl.querySelectorAll('input'));
-        console.log(inputEls);
-		 const shipInputs = inputEls.reduce((accum, inputEl) => {
-            accum[inputEl.name] = inputEl.value;
-            return accum;
-
-        }, {});
-
-    }
-    return {};
+    console.log(result);
 }//end of getShippingInfo()
 
 function setBillingInfo() {
@@ -29,15 +30,16 @@ function setBillingInputs(shippingInfo) {
 
     const formEl = document.querySelector('.js-billing-form');
 		if (formEl) {
+      const inputEl = Array.from(formEl.querySelectorAll('input'));
+     
 
-        	const inputEls = Array.from(formEl.querySelectorAll('input'));
-        	console.log(inputEls);
-        	inputEls.forEach(inputEl => {
-
-            inputEl.value = shippingInfo[inputEl.name]
-    }, {})
-    return {};
-}
+      inputEl.forEach(function(inputEl){
+          inputEl.value = shippingInfo[inputEl.name];
+      }, {});
+      return{}
+       
+    
+      }
 
 }//end of setBillingInputs() 
 
@@ -45,3 +47,14 @@ function setBillingInputs(shippingInfo) {
 
 const checkboxEl = document.getElementById("billAndShip");
 checkboxEl.addEventListener('change', setBillingInfo);
+
+
+/*
+1. In getShippingInfo() what is the benefit of putting the inputs and values into an object?
+2. In setBillingInputs() I'm trying to set the value of the input in the billing form with the 
+   value of the corresponding input in the shipping info form? I'm not understanding the 
+   setBillingInputs() and setBillingInfo() functions.
+3. When I console.log(result) in the getshippingInfo function it doesn't seem to log the zip code.
+   It is there if you expand the object in the console. Is that becasue it's a number and not a string?
+
+*/
