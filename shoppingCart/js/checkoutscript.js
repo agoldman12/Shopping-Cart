3b2c81c577b0f727d370f5fd17c1ba7613f40a24
Aaron <agoldman12@gmail.com>
2018-10-07 20:12:03 -0500

    Update checkoutscript.js
    I'm now using a for loop to iterate through the inputs of each form and set the value of the billing inputs equal to the value of the shipping inputs when the checkbox is checked. If the checkbox is unchecked the billing input values are set back to an empty string and focus is placed on the first name field.

--- shoppingCart/js/checkoutscript.js
+++ shoppingCart/js/checkoutscript.js
@@ -1,60 +1,34 @@
 'use strict';
 
-function getShippingInfo() {
+function copyShipping(){
+		  //get reference to the form elements
+		  const shipFormEl = document.querySelector('.js-shipping-form');
+		  const billFormEl = document.querySelector('.js-billing-form');
+		  
+    //check to see if shipping form is on the page
+  	if(shipFormEl){
+		  //get reference to all inputs in each form
+		  const shippingEls = shipFormEl.querySelectorAll('input');
+		  const billingEls = billFormEl.querySelectorAll('input');
+  		
+  		//if the checkbox is checked, set the billing input value = to shipping input value
+  		if(checkboxEl.checked){
+		     for(let i = 0; i < shippingEls.length; i++){
+		        billingEls[i].value = shippingEls[i].value;
+		     }
+	  	}else { //if checkbox is NOT checked, set billing inputs to empty string 
+		  	 for(let i = 0; i < shippingEls.length; i++){
+		        billingEls[i].value = "";
+		     }
+		    //put focus on first name field
+	     	billingEls[0].focus();
+	  }
+	}
 
-    const formEl = document.querySelector('.js-shipping-form');
-    const inputEl = Array.from(formEl.querySelectorAll('input'));
-    const initialValue = {};
-
-    const reducer = function(accum, inputEl){
-        if(formEl){
-          accum[inputEl.name] = inputEl.value;
-          return accum;  
-        }
-        
-    };
-    const result = inputEl.reduce(reducer, initialValue);
-    
-    console.log(result);
-}//end of getShippingInfo()
-
-function setBillingInfo() {
-    const shippingInfo = getShippingInfo();
-	
-	setBillingInputs(shippingInfo);
-
-}//end of setBillingInfo()
-
-
-function setBillingInputs(shippingInfo) {
-
-    const formEl = document.querySelector('.js-billing-form');
-		if (formEl) {
-      const inputEl = Array.from(formEl.querySelectorAll('input'));
-     
-
-      inputEl.forEach(function(inputEl){
-          inputEl.value = shippingInfo[inputEl.name];
-      }, {});
-      return{}
-       
-    
-      }
-
-}//end of setBillingInputs() 
+};//end copyShipping()
 
 
 
+//get reference to the checkbox and add the 'on change' event listener
 const checkboxEl = document.getElementById("billAndShip");
-checkboxEl.addEventListener('change', setBillingInfo);
-
-
-/*
-1. In getShippingInfo() what is the benefit of putting the inputs and values into an object?
-2. In setBillingInputs() I'm trying to set the value of the input in the billing form with the 
-   value of the corresponding input in the shipping info form? I'm not understanding the 
-   setBillingInputs() and setBillingInfo() functions.
-3. When I console.log(result) in the getshippingInfo function it doesn't seem to log the zip code.
-   It is there if you expand the object in the console. Is that becasue it's a number and not a string?
-
-*/
+checkboxEl.addEventListener('change', copyShipping);
